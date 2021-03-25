dac485f4596a2a3fc6e56506b4e06d3e59711d6e
SewellDinG <bigder.ding@gmail.com>
2021-03-25 18:51:29 +0800

    feat: Basic GUI

---
+++ unit1Impl.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/ying32/govcl/vcl"
+)
+
+//::private::
+type TForm1Fields struct {
+}
+
+func (f *TForm1) OnCheckClick(sender vcl.IObject) {
+    url := f.URL.Text()
+    if url == "" {
+        vcl.ShowMessage("None")
+        return
+    }
+    f.Output.SetText(url)
+}
